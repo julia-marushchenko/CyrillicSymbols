7788ca86b22956c4f732f415bbc4ce52ab4fd03a
Julia Marushchenko <marushchenko.yulia@gmail.com>
2025-05-16 18:34:06 +0200

    Add files via upload
    Committing Main class.
    "Hi" is written right, and "How are you" in strange sansckit coding (Unicode from https://developer.ibm.com/articles/l-u-cyr/.)

---
+++ src/com/kind/Main.java
@@ -0,0 +1,15 @@
+// Java program to print "Hi" and "How are you?" in ukrainian language
+// to console
+package com.kind;
+
+//Main class
+public class Main {
+
+    // Method main to run java program
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.println("\u041f\u0440\u0438\u0432\u0456\u0442");
+        System.out.println("\u0241\u0203\u043a \u0211\u0208\u0210\u0193\u0215\u0438\u003F" );
+    }
+}
